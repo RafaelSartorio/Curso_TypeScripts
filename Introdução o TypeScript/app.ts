b19de4499ed45968152936cb7e712b2d65a8fb14
RafaelSartorio <156337649+RafaelSartorio@users.noreply.github.com>
2024-07-10 12:23:23 -0300

    TypeScrtpt

---
+++ Introdução o TypeScript/app.ts
@@ -0,0 +1,20 @@
+// interface Pessoa { nome: string, idade: number , profissao ?: string }
+
+// const Rafael : Pessoa = {
+//     nome: 'Rafael',
+//     idade: 20
+// } 
+
+// const Gabriel: Pessoa ={
+//     nome: 'Gabriel',
+//     idade: 19,
+//     profissao: 'nada'
+// }
+
+// const arrayPessoa =  {
+//     Rafael,
+//     Gabriel
+   
+// }
+
+// console.log(arrayPessoa)
